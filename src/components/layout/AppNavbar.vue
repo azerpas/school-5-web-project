de159fe5319ccb39e5f3897e88ec63fcc11d6704
azerpas <xskylix@hotmail.fr>
2021-01-02 20:29:30 +0100

    Flexed Navbar

--- src/components/layout/AppNavbar.vue
+++ src/components/layout/AppNavbar.vue
@@ -1,19 +1,41 @@
 <template>
     <div id="nav">
         <CHeading as="h1"> Eido </CHeading>
-        <router-link to="/">I am an influencer</router-link> |
-        <router-link to="/login">Get Started</router-link> | 
-        <router-link to="/login">LogIn</router-link>
-        <CIconButton type="button" @click="$toggleColorMode" :icon="colorMode === 'light' ? 'moon' : 'sun'"></CIconButton>
+        <CBox display="inline" ml="auto">
+            <router-link to="/">I am an influencer</router-link> |
+            <router-link to="/login">Get Started</router-link> | 
+            <router-link to="/login">LogIn</router-link>
+            <CIconButton type="button" @click="$toggleColorMode" :icon="colorMode === 'light' ? 'moon' : 'sun'"></CIconButton>
+        </CBox>
     </div>
 </template>
 
 <script>
-    import { CIconButton, CHeading } from '@chakra-ui/vue';
+    import { CIconButton, CHeading, CBox } from '@chakra-ui/vue';
 
     export default {
         components:{
-            CIconButton, CHeading
-        }
+            CIconButton, CHeading, CBox
+        },
+        inject: ['$chakraColorMode', '$toggleColorMode'],
+        computed: {
+            colorMode () {
+                return this.$chakraColorMode()
+            }
+        },
     }
 </script>
+
+<style scoped>
+    #nav h1{
+        display: inline;
+    }
+    #nav {
+        display: grid;
+        grid-template-columns: 1fr 4fr;
+        align-items: center;
+    }
+    #nav *{
+        vertical-align: middle;
+    }
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,37 +1,39 @@
 <template>
-    <div class="register">
-        <CFlex align="center" justify="center">
-            <form class="form-auth">
-                <CStack spacing="5">
-                    <CHeading class="form-head">REGISTER</CHeading>
-                    <CFormControl is-required>
-                        <CFormLabel for="username">Email</CFormLabel>
-                        <CInputGroup>
-                            <CInputLeftElement><CIcon name="email" color="gray"/></CInputLeftElement>
-                            <CInput v-model="username" type="email" placeholder="youremail@gmail.com" id="username"/>
-                        </CInputGroup>
-                    </CFormControl>
-                    <CFormControl is-required>
-                        <CFormLabel for="password">Password</CFormLabel>
-                        <CInput v-model="password" type="password" id="password"/>
-                    </CFormControl>
-                    <CFormControl is-required>
-                        <CFormLabel for="rpassword">Repeat password</CFormLabel>
-                        <CInput v-model="repeatPassword" type="password" id="rpassword"/>
-                    </CFormControl>
-                    <CFormControl is-required>
-                        <CFormLabel for="category">Category</CFormLabel>
-                        <c-select v-model="category" placeholder="Choose your category" id="category">
-                            <option value="influencer">Influencer</option>
-                            <option value="business">Business</option>
-                        </c-select>
-                    </CFormControl>
-                    <CButton variantColor="indigo" :click="()=>{}" width="100%">Login</CButton>
-                    <p>Are you already registered? <router-link to="/login">Log In here!</router-link></p>
-                </CStack>
-            </form>
-        </CFlex>
-    </div>
+    <main>
+        <div class="register">
+            <CFlex align="center" justify="center">
+                <form class="form-auth">
+                    <CStack spacing="5">
+                        <CHeading class="form-head">REGISTER</CHeading>
+                        <CFormControl is-required>
+                            <CFormLabel for="username">Email</CFormLabel>
+                            <CInputGroup>
+                                <CInputLeftElement><CIcon name="email" color="gray"/></CInputLeftElement>
+                                <CInput v-model="username" type="email" placeholder="youremail@gmail.com" id="username"/>
+                            </CInputGroup>
+                        </CFormControl>
+                        <CFormControl is-required>
+                            <CFormLabel for="password">Password</CFormLabel>
+                            <CInput v-model="password" type="password" id="password"/>
+                        </CFormControl>
+                        <CFormControl is-required>
+                            <CFormLabel for="rpassword">Repeat password</CFormLabel>
+                            <CInput v-model="repeatPassword" type="password" id="rpassword"/>
+                        </CFormControl>
+                        <CFormControl is-required>
+                            <CFormLabel for="category">Category</CFormLabel>
+                            <c-select v-model="category" placeholder="Choose your category" id="category">
+                                <option value="influencer">Influencer</option>
+                                <option value="business">Business</option>
+                            </c-select>
+                        </CFormControl>
+                        <CButton variantColor="indigo" :click="()=>{}" width="100%">Login</CButton>
+                        <p>Are you already registered? <router-link to="/login">Log In here!</router-link></p>
+                    </CStack>
+                </form>
+            </CFlex>
+        </div>
+    </main>
 </template>
 
 <script>
