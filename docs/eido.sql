cf894441b8d2c1aa0cba33c67f142f992e59f6e6
azerpas <xskylix@hotmail.fr>
2021-01-04 16:18:42 +0100

    Added some ENUM to .sql

--- docs/eido.sql
+++ docs/eido.sql
@@ -1,73 +1,76 @@
+CREATE TYPE t_role AS ENUM ('ROLE_ADMIN','ROLE_USER','ROLE_INFLUENCER','ROLE_BRAND');
+CREATE TYPE t_unite AS ENUM ('video','minute','hour','photo','month','publication');
+
 CREATE TABLE Users(
-                      Id_Users COUNTER,
-                      name VARCHAR(50),
-                      firstName VARCHAR(50),
-                      password VARCHAR(50),
-                      email VARCHAR(50),
-                      bio VARCHAR(50),
-                      role VARCHAR(50),
-                      PRIMARY KEY(Id_Users),
-                      UNIQUE(email)
+    Id_Users COUNTER,
+    name VARCHAR(50),
+    firstName VARCHAR(50),
+    password VARCHAR(50),
+    email text,
+    bio text,
+    roles t_role,
+    PRIMARY KEY(Id_Users),
+    UNIQUE(email)
 );
 
 CREATE TABLE Work(
-                     Id_Work COUNTER,
-                     url VARCHAR(50),
-                     name VARCHAR(50),
-                     release_date VARCHAR(50),
-                     Id_Users INT NOT NULL,
-                     PRIMARY KEY(Id_Work),
-                     FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users)
+    Id_Work COUNTER,
+    url VARCHAR(50),
+    name VARCHAR(50),
+    Id_Users INT NOT NULL,
+    thumbnail text,
+    PRIMARY KEY(Id_Work),
+    FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users)
 );
 
 CREATE TABLE Keyword(
-                        Id_Keyword COUNTER,
-                        name VARCHAR(50),
-                        PRIMARY KEY(Id_Keyword)
+    Id_Keyword COUNTER,
+    name VARCHAR(50),
+    PRIMARY KEY(Id_Keyword)
 );
 
 CREATE TABLE Offer(
-                      Id_Offer COUNTER,
-                      price DECIMAL(15,2),
-                      unit VARCHAR(50),
-                      custom LOGICAL,
-                      Id_Users INT NOT NULL,
-                      PRIMARY KEY(Id_Offer),
-                      FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users)
+    Id_Offer COUNTER,
+    price DECIMAL(15,2),
+    unit t_unite,
+    custom LOGICAL,
+    Id_Users INT NOT NULL,
+    PRIMARY KEY(Id_Offer),
+    FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users)
 );
 
 CREATE TABLE Proposal(
-                         Id_Proposal COUNTER,
-                         release_date DATETIME,
-                         statut VARCHAR(50),
-                         Id_Users INT NOT NULL,
-                         Id_Offer INT NOT NULL,
-                         Id_Users_1 INT NOT NULL,
-                         PRIMARY KEY(Id_Proposal),
-                         UNIQUE(Id_Offer),
-                         FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
-                         FOREIGN KEY(Id_Offer) REFERENCES Offer(Id_Offer),
-                         FOREIGN KEY(Id_Users_1) REFERENCES Users(Id_Users)
+    Id_Proposal COUNTER,
+    release_date DATETIME,
+    statut VARCHAR(50),
+    Id_Users INT NOT NULL,
+    Id_Offer INT NOT NULL,
+    Id_Users_1 INT NOT NULL,
+    PRIMARY KEY(Id_Proposal),
+    UNIQUE(Id_Offer),
+    FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
+    FOREIGN KEY(Id_Offer) REFERENCES Offer(Id_Offer),
+    FOREIGN KEY(Id_Users_1) REFERENCES Users(Id_Users)
 );
 
 CREATE TABLE Platform(
-                         Id_Platform COUNTER,
-                         name VARCHAR(50),
-                         PRIMARY KEY(Id_Platform)
+    Id_Platform COUNTER,
+    name VARCHAR(50),
+    PRIMARY KEY(Id_Platform)
 );
 
 CREATE TABLE Users_Keyword(
-                              Id_Users INT,
-                              Id_Keyword INT,
-                              PRIMARY KEY(Id_Users, Id_Keyword),
-                              FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
-                              FOREIGN KEY(Id_Keyword) REFERENCES Keyword(Id_Keyword)
+    Id_Users INT,
+    Id_Keyword INT,
+    PRIMARY KEY(Id_Users, Id_Keyword),
+    FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
+    FOREIGN KEY(Id_Keyword) REFERENCES Keyword(Id_Keyword)
 );
 
 CREATE TABLE platform_user(
-                              Id_Users INT,
-                              Id_Platform INT,
-                              PRIMARY KEY(Id_Users, Id_Platform),
-                              FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
-                              FOREIGN KEY(Id_Platform) REFERENCES Platform(Id_Platform)
+    Id_Users INT,
+    Id_Platform INT,
+    PRIMARY KEY(Id_Users, Id_Platform),
+    FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users),
+    FOREIGN KEY(Id_Platform) REFERENCES Platform(Id_Platform)
 );
