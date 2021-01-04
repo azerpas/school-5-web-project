a7479e4bd5b549b9e7f92611d58d51f0faa04dd1
martin <martin.lenaerts@hotmail.fr>
2021-01-04 16:32:29 +0100

    maj sql et env.example

--- docs/eido.sql
+++ docs/eido.sql
@@ -2,7 +2,7 @@ CREATE TYPE t_role AS ENUM ('ROLE_ADMIN','ROLE_USER','ROLE_INFLUENCER','ROLE_BRA
 CREATE TYPE t_unite AS ENUM ('video','minute','hour','photo','month','publication');
 
 CREATE TABLE Users(
-    Id_Users COUNTER,
+    Id_Users int,
     name VARCHAR(50),
     firstName VARCHAR(50),
     password VARCHAR(50),
@@ -14,7 +14,7 @@ CREATE TABLE Users(
 );
 
 CREATE TABLE Work(
-    Id_Work COUNTER,
+    Id_Work int,
     url VARCHAR(50),
     name VARCHAR(50),
     Id_Users INT NOT NULL,
@@ -24,24 +24,24 @@ CREATE TABLE Work(
 );
 
 CREATE TABLE Keyword(
-    Id_Keyword COUNTER,
+    Id_Keyword int,
     name VARCHAR(50),
     PRIMARY KEY(Id_Keyword)
 );
 
 CREATE TABLE Offer(
-    Id_Offer COUNTER,
+    Id_Offer int,
     price DECIMAL(15,2),
     unit t_unite,
-    custom LOGICAL,
+    custom BOOLEAN,
     Id_Users INT NOT NULL,
     PRIMARY KEY(Id_Offer),
     FOREIGN KEY(Id_Users) REFERENCES Users(Id_Users)
 );
 
 CREATE TABLE Proposal(
-    Id_Proposal COUNTER,
-    release_date DATETIME,
+    Id_Proposal int,
+    release_date DATE,
     statut VARCHAR(50),
     Id_Users INT NOT NULL,
     Id_Offer INT NOT NULL,
@@ -54,7 +54,7 @@ CREATE TABLE Proposal(
 );
 
 CREATE TABLE Platform(
-    Id_Platform COUNTER,
+    Id_Platform int,
     name VARCHAR(50),
     PRIMARY KEY(Id_Platform)
 );
