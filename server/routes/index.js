f87aa43fc5d47a391a464aad4b4d36080ae60e6a
martin <martin.lenaerts@hotmail.fr>
2021-01-06 11:44:50 +0100

    modif prisma + ajout des TODO

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,10 @@ router.get('/', async (req, res) => {
     res.status(200).send({});
 });
 
+/**
+ * Route permettant de se connecter
+ * TODO : Ajout du user dans la session
+ */
 router.get('/login',async (req,res)=>{
     var email = req.query.email;
     var password = req.query.password;
@@ -34,19 +38,46 @@ router.get('/login',async (req,res)=>{
     }
 });
 
-
+/**
+ * Route recuperant les platformes d'un utilisateur (donné en parametre)
+ * Si il n'y a pas d'utilisateur : renvoi de toutes les platformes
+ */
 router.get("/platform",async (req,res)=>{
-    var idUser = parseInt(req.query.user_id);
-    const result = await prisma.user.findUnique({
-       where:{
-           id:idUser
-       }
-    }).Platform();
-
+    var idUser = req.query.user_id;
+    console.log(idUser)
+    if(idUser != undefined){
+        var result = await prisma.user.findUnique({
+            where:{
+                id:parseInt(idUser)
+            }
+        }).Platform();
+        console.log(result);
+    }else{
+        result = await prisma.platform.findMany({});
+    }
     res.send(result);
-    //const result = await prisma.user.findMany({});
 });
 
+/**
+ * Route affichant les proposition d'un utilisateur en fonction de son role
+ *
+ * TODO recup l'user dans la session et non dans la requete + select les propositions en fonction du role
+ */
+router.get("/proposal",async (req,res)=>{
+    var idUser = req.query.user_id;
+    var result = await prisma.user.findUnique({
+        where:{
+            id:parseInt(idUser)
+        }
+    }).Proposal_influencer();
+    console.log(result);
+    res.send(result);
+});
+
+
+/**
+ * Route enregistrant un nouvel utilisateur
+ */
 router.put("/user", async (req,res)=>{
    var email = req.query.email;
     var password = req.query.password;
@@ -75,9 +106,26 @@ router.put("/user", async (req,res)=>{
 
 });
 
-/*router.get("/get_password", async (req,res)=>{
-    bcrypt.hash(req.query.password, saltRounds, async function(err, hash) {
-        res.send(hash);
-    });
-});*/
+
+/**
+ *
+ * TODO : --OFFER--
+ *        - GET /offer (par user)
+ *        - PUT /offer
+ *        - DELETE /offer
+ *        - POST /offer
+ *        -
+ *        --PROPOSAL--
+ *        - PUT /proposal
+ *        - DELETE /proposal
+ *        -
+ *        --USER--
+ *        - DELETE /user
+ *        - POST /user
+ *        -
+ *        --KEYWORD--
+ *        - GET /keyword (par user)
+ *        - PUT /keyword (sauf si on decide que les keyword sont prédéfinis)
+ *        - DELETE /keyword (sauf si on decide que les keyword sont prédéfinis)
+ */
 module.exports = router
