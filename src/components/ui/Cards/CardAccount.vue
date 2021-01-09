14229f90176c82814613374cd911ed8a45527d59
azerpas <xskylix@hotmail.fr>
2021-01-09 20:08:19 +0100

    Working on Account page

---
+++ src/components/ui/Cards/CardAccount.vue
@@ -0,0 +1,19 @@
+<template>
+    <CBox bg="#7425F3" textAlign="center" borderRadius="lg" boxShadow="1px 1px 6px 0px">
+        <CImage :src="image" :alt="name+' profile picture'" m="auto" width="100%" roundedTop="lg"/>
+        <CHeading as="h3" textAlign="center" color="white">{{ user.firstname }}</CHeading>
+        <span v-for="keyword in keywords" :key="keyword">
+            <CText fontSize="xl" color="gray.200">{{ keyword }}</CText>
+        </span>
+    </CBox>
+</template>
+
+<script>
+import { CBox, CImage, CHeading, CText } from "@chakra-ui/vue";
+export default {
+    components: { CBox, CImage, CHeading, CText },
+    props:{
+        user: {id: null, email: null, bio: null, firstname: null, name: null, roles: null}
+    }
+}
+</script>
--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,9 +1,36 @@
 <template>
-    <main></main>
+    <main>
+        <CGrid 
+            :template-columns="{base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)'}" 
+            :gap="{base: '5', sm: '3'}"
+            mt="3"
+        >
+            <CardAccount :user="user"/>
+            <CBox>
+                <CHeading>MY WORK</CHeading>
+            </CBox>
+        </CGrid>
+        
+    </main>
 </template>
 
 <script>
+import CardAccount from "../components/ui/Cards/CardAccount";
+import {CBox, CHeading, CGrid} from "@chakra-ui/vue";
 export default {
-    
+    components: {
+        CardAccount,
+        CBox, CHeading, CGrid
+    },
+    computed: {
+        getWork(){
+            // TODO: Request to get works of user, req.session will fetch his id etc... no need to send it
+        }
+    }, 
+    data(){
+        return{
+            user: {id: null, email: null, bio: null, firstname: null, name: null, roles: null}
+        }
+    },
 }
 </script>
