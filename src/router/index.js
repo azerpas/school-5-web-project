71b0975ea8ef6441330cd111e14635e95a445f63
azerpas <xskylix@hotmail.fr>
2021-01-09 19:55:36 +0100

    Added 404 support

--- src/router/index.js
+++ src/router/index.js
@@ -47,10 +47,19 @@ const routes = [
   {
     path: '/influencers',
     name: 'Influencers',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/influencers/Index.vue'),
+    props: (route) => ({ type: route.query.type })
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Account.vue'),
+    props: (route) => ({ type: route.query.type })
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: () => import(/* webpackChunkName: "about" */ '../views/404.vue'),
     props: (route) => ({ type: route.query.type })
   }
 ]
