73682b832a69c3fb955e9beedb30fb99546872eb
azerpas <xskylix@hotmail.fr>
2021-01-09 19:39:32 +0100

    Styled CardUser

--- src/views/brands/Index.vue
+++ src/views/brands/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <div class="brands">
-            <CBox :mx="{base: 2}">
+            <CBox :mx="{base: 2}" >
                 <CHeading as="h1" textAlign="center">Our Partners</CHeading>
                 <CHeading as="h4" textAlign="center" size="4" fontWeight="300">Choose from a wide range of diverse content creators</CHeading>
                 <CBox mt="3">
@@ -12,10 +12,12 @@
                         <option value="sport">Sport</option>
                     </CSelect>
                 </CBox>
-                <CGrid>
-                    <CardUser>
-
-                    </CardUser>
+                <CGrid 
+                    :template-columns="{base: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)', lg: 'repeat(4, 1fr)'}" 
+                    :gap="{base: '5', sm: '3'}"
+                    mt="3"
+                >
+                    <CardUser/>
                 </CGrid>
             </CBox>
         </div>
@@ -29,6 +31,11 @@ export default {
     components:{
         CBox, CHeading, CSelect, CGrid,
         CardUser
+    }, 
+    data(){
+        return{ 
+            category: null
+        }
     }
 }
 </script>
