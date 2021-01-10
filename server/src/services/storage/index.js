24fdc54d09b61049432f5955e3ad6fd660d141e2
azerpas <xskylix@hotmail.fr>
2021-01-10 10:22:30 +0100

    Added Jest to test locally and reworked upload file function

--- server/src/services/storage/index.js
+++ server/src/services/storage/index.js
@@ -1,7 +1,7 @@
-import CloudStorage from "@google-cloud/storage";
+import {Storage} from "@google-cloud/storage";
+import { v4 as uuidv4 } from 'uuid';
 
 export const uploadFile = async (filename) => {
-    const { Storage } = CloudStorage;
     const storage = new Storage({
         credentials: {
             private_key: process.env.PRIVATE_KEY,
@@ -10,9 +10,12 @@ export const uploadFile = async (filename) => {
         projectId: process.env.PROJECT_ID
     });
     const bucket = storage.bucket(process.env.BUCKET_NAME);
+    const fileUuid = uuidv4();
     const res = await bucket.upload(filename, {
+        destination: fileUuid,
         metadata: {
             cacheControl: 'public, max-age=31536000',
         }
-    })
+    });
+    return res[0].name;
 }
