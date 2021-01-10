0e9aba2f2dea07fa0b2b9634861a919a2c57fc3d
azerpas <xskylix@hotmail.fr>
2021-01-10 19:03:26 +0100

    Hotfix unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -36,21 +36,6 @@ const routes = [
     props: (route) => ({ type: route.query.type })
   },
   {
-    path: '/brands',
-    name: 'Brands',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/brands/Index.vue'),
-    props: (route) => ({ type: route.query.type })
-  },
-  {
-    path: '/influencers',
-    name: 'Influencers',
-    component: () => import(/* webpackChunkName: "about" */ '../views/influencers/Index.vue'),
-    props: (route) => ({ type: route.query.type })
-  },
-  {
     path: '/account',
     name: 'Account',
     component: () => import(/* webpackChunkName: "about" */ '../views/Account.vue'),
